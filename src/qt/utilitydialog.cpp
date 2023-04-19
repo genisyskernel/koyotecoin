b8ec037eedadf1b897be747e75aae58ddb5a3730
Eduardo Doege <eduardo.doege@outlook.com>
2023-04-19 08:53:22 -0300

    koyotecoin: fix ui form - utilitydialog.cpp
    Koyotecoin: Fix UI Form - utilitydialog.cpp

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -37,71 +37,72 @@ HelpMessageDialog::HelpMessageDialog(QWidget* parent, bool about) : QDialog(pare
 
     QString version = QString{PACKAGE_NAME} + " " + tr("version") + " " + QString::fromStdString(FormatFullVersion());
 
-    if (about) {
-        setWindowTitle(tr("About %1").arg(PACKAGE_NAME));
+    // if (about) {
+    setWindowTitle(tr("About %1").arg(PACKAGE_NAME));
 
-        std::string licenseInfo = LicenseInfo();
-        /// HTML-format the license message from the core
-        QString licenseInfoHTML = QString::fromStdString(LicenseInfo());
-        // Make URLs clickable
-        QRegularExpression uri(QStringLiteral("<(.*)>"), QRegularExpression::InvertedGreedinessOption);
-        licenseInfoHTML.replace(uri, QStringLiteral("<a href=\"\\1\">\\1</a>"));
-        // Replace newlines with HTML breaks
-        licenseInfoHTML.replace("\n", "<br>");
+    std::string licenseInfo = LicenseInfo();
+    /// HTML-format the license message from the core
+    QString licenseInfoHTML = QString::fromStdString(LicenseInfo());
+    // Make URLs clickable
+    QRegularExpression uri(QStringLiteral("<(.*)>"), QRegularExpression::InvertedGreedinessOption);
+    licenseInfoHTML.replace(uri, QStringLiteral("<a href=\"\\1\">\\1</a>"));
+    // Replace newlines with HTML breaks
+    licenseInfoHTML.replace("\n", "<br>");
 
-        // ui->aboutMessage->setTextFormat(Qt::RichText);
-        // ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-        text = version + "\n" + QString::fromStdString(FormatParagraph(licenseInfo));
-        ui->aboutMessage->setText(version + "<br><br>" + licenseInfoHTML);
-        // ui->aboutMessage->setWordWrap(true);
-        // ui->helpMessage->setVisible(false);
-    } else {
-        setWindowTitle(tr("Command-line options"));
-        QString header = "Usage:  koyotecoin-qt [command-line options]                     \n";
-        // QTextCursor cursor(ui->helpMessage->document());
-        cursor.insertText(version);
-        cursor.insertBlock();
-        cursor.insertText(header);
-        cursor.insertBlock();
+    // ui->aboutMessage->setTextFormat(Qt::RichText);
+    // ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    text = version + "\n" + QString::fromStdString(FormatParagraph(licenseInfo));
+    ui->aboutMessage->setText(version + "<br><br>" + licenseInfoHTML);
+    // ui->aboutMessage->setWordWrap(true);
+    // ui->helpMessage->setVisible(false);
 
-        std::string strUsage = gArgs.GetHelpMessage();
-        QString coreOptions = QString::fromStdString(strUsage);
-        text = version + "\n\n" + header + "\n" + coreOptions;
+    // } else {
+    //     setWindowTitle(tr("Command-line options"));
+    //     QString header = "Usage:  koyotecoin-qt [command-line options]                     \n";
+    //     // QTextCursor cursor(ui->helpMessage->document());
+    //     cursor.insertText(version);
+    //     cursor.insertBlock();
+    //     cursor.insertText(header);
+    //     cursor.insertBlock();
 
-        QTextTableFormat tf;
-        tf.setBorderStyle(QTextFrameFormat::BorderStyle_None);
-        tf.setCellPadding(2);
-        QVector<QTextLength> widths;
-        widths << QTextLength(QTextLength::PercentageLength, 35);
-        widths << QTextLength(QTextLength::PercentageLength, 65);
-        tf.setColumnWidthConstraints(widths);
+    //     std::string strUsage = gArgs.GetHelpMessage();
+    //     QString coreOptions = QString::fromStdString(strUsage);
+    //     text = version + "\n\n" + header + "\n" + coreOptions;
 
-        QTextCharFormat bold;
-        bold.setFontWeight(QFont::Bold);
+    //     QTextTableFormat tf;
+    //     tf.setBorderStyle(QTextFrameFormat::BorderStyle_None);
+    //     tf.setCellPadding(2);
+    //     QVector<QTextLength> widths;
+    //     widths << QTextLength(QTextLength::PercentageLength, 35);
+    //     widths << QTextLength(QTextLength::PercentageLength, 65);
+    //     tf.setColumnWidthConstraints(widths);
 
-        for (const QString& line : coreOptions.split("\n")) {
-            if (line.startsWith("  -")) {
-                cursor.currentTable()->appendRows(1);
-                cursor.movePosition(QTextCursor::PreviousCell);
-                cursor.movePosition(QTextCursor::NextRow);
-                cursor.insertText(line.trimmed());
-                cursor.movePosition(QTextCursor::NextCell);
-            } else if (line.startsWith("   ")) {
-                cursor.insertText(line.trimmed() + ' ');
-            } else if (line.size() > 0) {
-                // Title of a group
-                if (cursor.currentTable())
-                    cursor.currentTable()->appendRows(1);
-                cursor.movePosition(QTextCursor::Down);
-                cursor.insertText(line.trimmed(), bold);
-                cursor.insertTable(1, 2, tf);
-            }
-        }
+    //     QTextCharFormat bold;
+    //     bold.setFontWeight(QFont::Bold);
 
-        // ui->helpMessage->moveCursor(QTextCursor::Start);
-        // ui->scrollArea->setVisible(false);
-        ui->aboutLogo->setVisible(false);
-    }
+    //     for (const QString& line : coreOptions.split("\n")) {
+    //         if (line.startsWith("  -")) {
+    //             cursor.currentTable()->appendRows(1);
+    //             cursor.movePosition(QTextCursor::PreviousCell);
+    //             cursor.movePosition(QTextCursor::NextRow);
+    //             cursor.insertText(line.trimmed());
+    //             cursor.movePosition(QTextCursor::NextCell);
+    //         } else if (line.startsWith("   ")) {
+    //             cursor.insertText(line.trimmed() + ' ');
+    //         } else if (line.size() > 0) {
+    //             // Title of a group
+    //             if (cursor.currentTable())
+    //                 cursor.currentTable()->appendRows(1);
+    //             cursor.movePosition(QTextCursor::Down);
+    //             cursor.insertText(line.trimmed(), bold);
+    //             cursor.insertTable(1, 2, tf);
+    //         }
+    //     }
+
+    //     // ui->helpMessage->moveCursor(QTextCursor::Start);
+    //     // ui->scrollArea->setVisible(false);
+    //     ui->aboutLogo->setVisible(false);
+    // }
 
     GUIUtil::handleCloseWindowShortcut(this);
 }
